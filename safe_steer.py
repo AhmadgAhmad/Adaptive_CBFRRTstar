ed25578d2ed2a30becb392ca3942f6fbbbc295d9
AhmadgAhmad <ahmadgh@bu.edu>
2021-12-15 12:18:05 -0500

    First commit

---
+++ safe_steer.py
@@ -0,0 +1,15 @@
+"""
+Author: Ahmad Ahmad, ahmadgh@bu.edu
+This class is define CBF-QP, and CLF-CBF-QP local trajectory planner for (Adaptive) CBF-RRT* algorithm 
+"""
+import numpy as np 
+import agent,goal,dynamics
+
+class Safe_steer:
+    
+    def __init__(self) -> None:
+        pass
+
+
+
+
